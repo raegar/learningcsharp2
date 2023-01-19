554dfdc5ff2f234a8bb087979e7c148a8a446ec5
Jamie Myland <jamie.myland@aru.ac.uk>
2023-01-19 15:48:45 +0000

    Changed the program to support double numbers

--- Program.cs
+++ Program.cs
@@ -3,12 +3,12 @@ class Program //Write a C# Sharp program to print the sum of two numbers.
 {
     static void Main(string[] args)
     {
-        int firstNumber = 7;
-        int secondNumber = 12;
+        double firstNumber = 7;
+        double secondNumber = 12;
         Console.Write("Enter the first number: ");
-        firstNumber = Convert.ToInt32( Console.ReadLine() );
+        firstNumber = Convert.ToDouble( Console.ReadLine() );
         Console.Write("Enter the second number: ");
-        secondNumber = Convert.ToInt32( Console.ReadLine() );
+        secondNumber = Convert.ToDouble( Console.ReadLine() );
 
         Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is {firstNumber + secondNumber}");
         Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
