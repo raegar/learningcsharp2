12d3a914335baa4305a952baf1582e8c5f8f046a
Jamie Myland <jamie.myland@aru.ac.uk>
2023-01-19 15:27:35 +0000

    Changed numbers to variables

--- Program.cs
+++ Program.cs
@@ -3,7 +3,10 @@ class Program //Write a C# Sharp program to print the sum of two numbers.
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(7+12);
+        int firstNumber = 7;
+        int secondNumber = 12;
+
+        Console.WriteLine(firstNumber + secondNumber);
         Console.ReadKey(true);
     }
 }
