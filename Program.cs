6fb9052c1b6a187e64e78c643a064769bc70d30d
Jamie Myland <jamie.myland@aru.ac.uk>
2023-01-19 15:45:22 +0000

    Prompt the user to enter two numbers

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@ class Program //Write a C# Sharp program to print the sum of two numbers.
     {
         int firstNumber = 7;
         int secondNumber = 12;
+        Console.Write("Enter the first number: ");
+        firstNumber = Convert.ToInt32( Console.ReadLine() );
+        Console.Write("Enter the second number: ");
+        secondNumber = Convert.ToInt32( Console.ReadLine() );
 
         Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is {firstNumber + secondNumber}");
         Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
